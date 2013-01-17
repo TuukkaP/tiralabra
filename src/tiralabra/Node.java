7a1f2eff42501aca2fbcd56c9cdd122b76840489
TuukkaP <tuukka.peuraniemi@gmail.com>
2013-01-17 11:15:20 +0200

    Tiralabraprojektin perustus

---
+++ src/tiralabra/Node.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra;
+
+/**
+ *
+ * @author tuukka
+ */
+public class Node {
+    
+    /**
+     * Solmun perustus
+     */
+
+    public Node() {
+    }
+    
+    /**
+     * Solmujen vertailu
+     * @param alku
+     * @param maali
+     * @return Jos negatiivinen alku on parempi, jos positiivinen maali on parempi.
+     */
+    public int compareTo (Node alku, Node maali) {
+    
+        return 0;
+    }
+}
---
+++ src/tiralabra/Tiralabra.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra;
+
+/**
+ *
+ * @author tuukka
+ */
+public class Tiralabra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    /**
+     *  Djikstran algoritmi
+     */
+    private static void djikstra() {
+        
+    }
+    
+    /**
+     * A* -algoritmi
+     */
+    
+    private static void astar() {
+        
+    }
+    
+    /**
+     * A*-heuristiikka numero 1
+     * Euklidinen etäisyys
+     */
+    
+    private static void heur1() {
+        
+    }
+    
+        /**
+     * A*-heuristiikka numero 2
+     * Oma heuristiikka
+     */
+    
+    private static void heur2() {
+        
+    }
+}
