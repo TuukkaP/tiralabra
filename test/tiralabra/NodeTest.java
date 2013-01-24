4e8d4e3881d5756e95823d0d8b8f6a6a50d43746
TuukkaP <tuukka.peuraniemi@gmail.com>
2013-01-24 22:04:54 +0200

    Junit ja viikkoraportti

---
+++ test/tiralabra/NodeTest.java
@@ -0,0 +1,324 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tuukka
+ */
+public class NodeTest {
+    
+    public NodeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getVari method, of class Node.
+     */
+    @Test
+    public void testGetVari() {
+        System.out.println("getVari");
+        Node instance = null;
+        int expResult = 0;
+        int result = instance.getVari();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setVari method, of class Node.
+     */
+    @Test
+    public void testSetVari() {
+        System.out.println("setVari");
+        int vari = 0;
+        Node instance = null;
+        instance.setVari(vari);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setMaali method, of class Node.
+     */
+    @Test
+    public void testSetMaali() {
+        System.out.println("setMaali");
+        boolean maali = false;
+        Node instance = null;
+        instance.setMaali(maali);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setMaaliSolmu method, of class Node.
+     */
+    @Test
+    public void testSetMaaliSolmu() {
+        System.out.println("setMaaliSolmu");
+        Node maali = null;
+        Node instance = null;
+        instance.setMaaliSolmu(maali);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setLahto method, of class Node.
+     */
+    @Test
+    public void testSetLahto() {
+        System.out.println("setLahto");
+        boolean lahto = false;
+        Node instance = null;
+        instance.setLahto(lahto);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEdellinen method, of class Node.
+     */
+    @Test
+    public void testGetEdellinen() {
+        System.out.println("getEdellinen");
+        Node instance = null;
+        Node expResult = null;
+        Node result = instance.getEdellinen();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPaino method, of class Node.
+     */
+    @Test
+    public void testGetPaino() {
+        System.out.println("getPaino");
+        Node instance = null;
+        int expResult = 0;
+        int result = instance.getPaino();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getX method, of class Node.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println("getX");
+        Node instance = null;
+        int expResult = 0;
+        int result = instance.getX();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getY method, of class Node.
+     */
+    @Test
+    public void testGetY() {
+        System.out.println("getY");
+        Node instance = null;
+        int expResult = 0;
+        int result = instance.getY();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isKayty method, of class Node.
+     */
+    @Test
+    public void testIsKayty() {
+        System.out.println("isKayty");
+        Node instance = null;
+        boolean expResult = false;
+        boolean result = instance.isKayty();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isNahty method, of class Node.
+     */
+    @Test
+    public void testIsNahty() {
+        System.out.println("isNahty");
+        Node instance = null;
+        boolean expResult = false;
+        boolean result = instance.isNahty();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEdellinen method, of class Node.
+     */
+    @Test
+    public void testSetEdellinen() {
+        System.out.println("setEdellinen");
+        Node edellinen = null;
+        Node instance = null;
+        instance.setEdellinen(edellinen);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setKayty method, of class Node.
+     */
+    @Test
+    public void testSetKayty() {
+        System.out.println("setKayty");
+        boolean kayty = false;
+        Node instance = null;
+        instance.setKayty(kayty);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNahty method, of class Node.
+     */
+    @Test
+    public void testSetNahty() {
+        System.out.println("setNahty");
+        boolean nahty = false;
+        Node instance = null;
+        instance.setNahty(nahty);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPaino method, of class Node.
+     */
+    @Test
+    public void testSetPaino() {
+        System.out.println("setPaino");
+        int paino = 0;
+        Node instance = null;
+        instance.setPaino(paino);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setX method, of class Node.
+     */
+    @Test
+    public void testSetX() {
+        System.out.println("setX");
+        int x = 0;
+        Node instance = null;
+        instance.setX(x);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setY method, of class Node.
+     */
+    @Test
+    public void testSetY() {
+        System.out.println("setY");
+        int y = 0;
+        Node instance = null;
+        instance.setY(y);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Node.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Node instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of compareTo method, of class Node.
+     */
+    @Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+        Node o = null;
+        Node instance = null;
+        int expResult = 0;
+        int result = instance.compareTo(o);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of heuristiikka method, of class Node.
+     */
+    @Test
+    public void testHeuristiikka() {
+        System.out.println("heuristiikka");
+        Node solmu = null;
+        Node instance = null;
+        double expResult = 0.0;
+        double result = instance.heuristiikka(solmu);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isMaali method, of class Node.
+     */
+    @Test
+    public void testIsMaali() {
+        System.out.println("isMaali");
+        Node instance = null;
+        boolean expResult = false;
+        boolean result = instance.isMaali();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
